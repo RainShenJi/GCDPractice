bbaf28014c560b824332849af54895c4a9b36df1
RainShenJi <605356244@qq.com>
2015-09-05 10:44:35 +0800

    新增
    后续的代码

--- GCDPractice/ViewController.m
+++ GCDPractice/ViewController.m
@@ -51,7 +51,157 @@
 //串行并行主要影响的是执行任务的方式
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     
-    [self asyncGlobalQueue];
+   // [self asyncGlobalQueue];
+  //  [self readingWrittingHandle];
+  //  [self applyDispatch];
+//  [self suspendAndResume];
+    [self semaphoreDispatch];
+    [self sourceDispatch];
+}
+
+- (void)sourceDispatch {
+
+    
+    
+}
+
+- (void)semaphoreDispatch {
+
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    NSMutableArray * array = [NSMutableArray array];
+//    for (int i = 0; i < 100000; i++) {
+//        
+//        dispatch_async(queue, ^{
+//            
+//            [array addObject:[NSNumber numberWithInt:i]];
+//        });
+//    }
+    /**
+     *  生成 Dispatch Semaphore
+     *
+     *  @param 1 初始值设为1，保证可访问NSMutableArray对象的线程同时只有1个
+     *
+     */
+    dispatch_semaphore_t semaphore = dispatch_semaphore_create(1);
+    for (int i = 0; i < 100000; i++) {
+        
+        dispatch_async(queue, ^{
+            
+            dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+            [array addObject:[NSNumber numberWithInt:i]];
+            dispatch_semaphore_signal(semaphore);
+        });
+        
+    }
+    NSLog(@"%@",array);
+}
+
+- (void)suspendAndResume {
+
+    dispatch_queue_t queue = dispatch_queue_create("com.exmple", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_queue_t queue1 = dispatch_queue_create("com.exmple1", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_set_target_queue(queue1, queue);
+    dispatch_suspend(queue1);
+    dispatch_async(queue1, ^{
+        
+        NSLog(@"数据库读取操作1");
+    });
+    dispatch_async(queue, ^{
+        
+        NSLog(@"数据库读取操作0");
+    });
+    dispatch_async(queue, ^{
+        
+        NSLog(@"数据库读取操作2");
+    });
+    dispatch_resume(queue1);
+
+}
+
+- (void)applyDispatch {
+    
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    //dispatch_queue_t queue = dispatch_queue_create("com.gcd", NULL);
+    NSArray * array = @[@1,@2,@3,@4,@5];
+    dispatch_async(queue, ^{
+        dispatch_apply([array count], queue, ^(size_t index) {
+           
+            NSLog(@"%zu: %@", index, array[index]);
+        });
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+           
+            NSLog(@"Done");
+        });
+        
+    });
+}
+
+- (void)readingWrittingHandle {
+
+    dispatch_queue_t queue = dispatch_queue_create("com.exmple", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_async(queue, ^{
+       
+        NSLog(@"数据库读取操作0");
+    });
+    dispatch_async(queue, ^{
+        
+        NSLog(@"数据库读取操作1");
+    });
+    dispatch_async(queue, ^{
+        
+        NSLog(@"数据库读取操作2");
+    });
+    dispatch_async(queue, ^{
+        
+        NSLog(@"数据库读取操作3");
+    });
+    /**
+     *  写入处理
+     *  将写入的内容读取之后的处理中
+     */
+//    dispatch_async(queue, ^{
+//        
+//        NSLog(@"数据库写入操作");
+//    });
+    /**
+     *  像上面这样写肯定是不行的，因为可能在写入处理的前面读到了与期望不符的数据，或者后面有的可能读不到数据，还可能因为非法访问导致应用程序异常结束，也可能有数据竞争问题等等，那么我们应该怎么做呢？这是就可以用dispatch_barrier_async函数了
+     */
+    dispatch_barrier_async(queue, ^{
+       
+        NSLog(@"数据库写入操作");
+    });
+
+    /**
+     *  barrier翻译成障碍物分界线的意思
+     *  dispatch_barrier_async以它为分界线，这个函数会把等待追加到Concurrent Dispatch Queue中的执行处理结束后，把block的任务添加到队列中，等这个block中的任务执行结束之后，Concurrent Dispatch Queue才恢复一般的动作，追加到Concurrent Dispatch Queue中的任务又开始处理
+     */
+    dispatch_async(queue, ^{
+        
+        NSLog(@"数据库读取操作4");
+    });
+    dispatch_async(queue, ^{
+        
+        NSLog(@"数据库读取操作5");
+    });
+    dispatch_async(queue, ^{
+        
+        NSLog(@"数据库读取操作6");
+    });
+    dispatch_async(queue, ^{
+        
+        NSLog(@"数据库读取操作7");
+    });
+    dispatch_async(queue, ^{
+        
+        NSLog(@"数据库读取操作8");
+    });
+    dispatch_async(queue, ^{
+        
+        NSLog(@"数据库读取操作9");
+    });
+
+
 }
 
 - (void)asyncGlobalQueue {
@@ -88,8 +238,8 @@
     //队列组的一个好处就是凡是扔到队列组中的任务，它会等队列组中的所有任务都搞定后，他会调用下面的这个API
     //合并图片
     //唤醒
-    dispatch_group_notify(group, globalQueue, ^{
-       
+    long result = dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
+    if (result == 0) {
         UIGraphicsBeginImageContextWithOptions(image.size, NO, 0.0);
         
         //绘制第一张图片
@@ -108,7 +258,31 @@
         dispatch_async(dispatch_get_main_queue(), ^{
             _fullImageView.image = fullImage;
         });
-    });
+    }else{
+        
+        NSLog(@"某一个处理还在进行中");
+    }
+//    dispatch_group_notify(group, globalQueue, ^{
+//       
+//        UIGraphicsBeginImageContextWithOptions(image.size, NO, 0.0);
+//        
+//        //绘制第一张图片
+//        [image drawInRect:CGRectMake(0, 0, image.size.width, image.size.height)];
+//        
+//        //绘制第二张图片
+//        [image2 drawInRect:CGRectMake(340, image.size.height-image2.size.height*0.5, image2.size.width*0.5, image2.size.height*0.5)];
+//        
+//        //得到上下文中的图片
+//        UIImage * fullImage = UIGraphicsGetImageFromCurrentImageContext();
+//        
+//        //结束上下文
+//        UIGraphicsEndImageContext();
+//        
+//        //3.回到主线程刷新UI
+//        dispatch_async(dispatch_get_main_queue(), ^{
+//            _fullImageView.image = fullImage;
+//        });
+//    });
 //    dispatch_async(globalQueue, ^{
 //        
 //        dispatch_async(dispatch_get_main_queue(), ^{
